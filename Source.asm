97d85acb9144b5bdd27dcb3b52e4ef7ee533bb2d
jmeegan2 <jmeegan2@owlmail.harford.edu>
2021-04-29 02:35:09 -0400

    done for the day

---
+++ Source.asm
@@ -0,0 +1,87 @@
+; EXTERNAL DEPENDENCIES
+;So this is probrably a good point to start at 4/29
+
+INCLUDE		Irvine32.inc
+INCLUDELIB	Irvine32.lib
+
+; EXECUTION MODE PARAMETERS
+.386
+.model flat, stdcall
+.stack 4096
+
+; PROTOTYPES
+ExitProcess PROTO, dwExitCode:DWORD
+
+; SYMBOLIC CONSTANTS
+NUMBER_OF_COLORS =		16d
+
+; DATA SEGMENT
+.data
+foregroundColorCounter		BYTE	?
+backgroundColorCounter		BYTE	?
+sampleText					BYTE	"                                  ", 0Dh, 0Ah, 0
+string_Horizontal			BYTE	"+0    +1    +2    +3    +4    +5    +6    +7    +8    +9   +10    +11    +12    +13    +14    +15", 0Dh, 0Ah, 0
+; CODE SEGMENT
+.code
+; NOTE --------------------------------------------------------------------------------------------
+; If you are acustom to programming in a C++ or Java environment, consider adopting the following
+;  conventional pattern for structuring nested loops in Assembly.  The use of tabs to provide a
+;  visual cue relating to how deep in the nested structure an instruction is can be very useful
+;  for overall code readability.
+; -------------------------------------------------------------------------------------------------
+main PROC
+mov edx, offset string_Horizontal
+call writestring
+	; Set up variables for OUTER loop.
+	mov ECX, NUMBER_OF_COLORS
+	mov backgroundColorCounter, 0
+
+	
+	backgroundLoop:
+		; Set the background color in AL.
+		movzx EAX, backgroundColorCounter
+		
+		; Shift the background color to the upper half of AL (where it belongs).
+		shl EAX, 2
+
+		; Preserve ECX contents upon entering INNER loop.
+		push ECX
+		
+		; Set up variables for INNER loop.
+		mov ECX, NUMBER_OF_COLORS
+		mov foregroundColorCounter, 0
+				foregroundLoop:
+			; Set the newly forged text color and display the sample message.
+			call SetTextColor
+			call PrintSampleText		
+
+
+			; Move onto the next foreground color.
+			inc AL
+		loop foregroundLoop
+		
+		; Restore the ECX contents to allow OUTER loop to continue flowing naturally.
+		pop ECX
+
+		; Move into the next background color.
+		inc backgroundColorCounter
+	loop backgroundLoop
+	
+	; Indicate normal termination to OS.
+	invoke ExitProcess, 0
+main ENDP
+
+PrintSampleText PROC
+	; This procedure uses EDX internally, so preverve it.
+	push EDX
+	
+	; Write the text.
+	mov EDX, OFFSET sampleText
+	call WriteString
+
+	; Restore EDX upon procedure completion.
+	pop EDX
+	ret
+PrintSampleText ENDP
+
+END main		; End of program OPCODES.
