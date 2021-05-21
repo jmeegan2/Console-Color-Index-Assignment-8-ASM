afa4e59a85b02134be3fe2cd75ffd124cd4c1e7a
jmeegan2 <jmeegan2@owlmail.harford.edu>
2021-05-20 23:31:42 -0400

    changed formatting slightly but now im gonna turn this in

--- Source.asm
+++ Source.asm
@@ -29,31 +29,31 @@ topText		DB "    +0  +1  +2  +3  +4  +5  +6  +7  +8  +9  +10 +11 +12 +13 +14 +15
 .code
 main PROC
 	MOV EDX, OFFSET topText		
-	CALL WriteString				; Prints top text
+	CALL WriteString					; Prints top text
 
-	MOV ECX, 16						; Prime ecx for background color loop
+	MOV ECX, 16							; Prime ecx for background color loop
 	MOV EDX, OFFSET Letter_X_Text		; Prime EDX for printing
 
 	backgroundLoop:
 
 		MOVZX EAX, counterBackGround		; Move BackGround number to EAX
-		CALL WriteDec				; Print ^
+		CALL WriteDec						; Print ^
 
-		PUSH ECX					; Preserve count for outside loop
-		MOV ECX, 16					; Set number for inside loop
+		PUSH ECX							; Preserve count for outside loop
+		MOV ECX, 16							; Set number for inside loop
 		foregroundLoop:
 			
 			MOVZX EAX, counterBackGround	; Move BackGround number to EAX
-			SHl EAX, 4				; Move it to the right part
+			SHl EAX, 4						; Move it to the right part
 			MOV AL, counterForeGround		; Move the Foreground color into the right place
-			CALL SetTextColor		; Change the color
-			CALL WriteString		; Print
+			CALL SetTextColor				; Change the color
+			CALL WriteString				; Print
 			INC counterForeGround			; Shift to next Foreground color
 		
 		LOOP foregroundLoop
 		
 		POP ECX						; Return to outside loop number
-		ADD counterBackGround, 16			; Increment BackGround color
+		ADD counterBackGround, 16	; Increment BackGround color
 		MOV EAX, 7					
 		CALL SetTextColor			; Set to default console color
 		CALL Crlf
